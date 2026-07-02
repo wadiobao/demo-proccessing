838d3a12a9dcb35b348e0dd580d165cf8b17b412
Bao Duong Q <dumabao69@gmail.com>
2026-07-02 18:58:44 +0700

    refactor: rename classes for consistency

--- src/main/java/com/example/demo/modules/document/upload/application/query/GetDocumentQuery.java
+++ src/main/java/com/example/demo/modules/document/upload/application/query/GetDocumentQuery.java
@@ -1,5 +1,11 @@
 package com.example.demo.modules.document.upload.application.query;
 
-public class GetDocumentQuery {
+import lombok.Builder;
+import lombok.Getter;
 
+// TODO FIXME: Đổi tên thành GetDocumentQuery
+@Getter
+@Builder
+public class GetPdfQuery {
+    private final Long id;
 }
